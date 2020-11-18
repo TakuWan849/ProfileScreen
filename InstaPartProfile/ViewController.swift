18ea3431d242ec8246a1d567ebc7b7f09b04adfb
TakuWan849 <wantaku339@gmail.com>
2020-11-18 22:33:40 +0900

    Initial Commit

---
+++ InstaPartProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPartProfile
+//
+//  Created by 野澤拓己 on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
