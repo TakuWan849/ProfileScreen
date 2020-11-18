ac13b6239610e3ca34d4913098b5960a654f9f93
TakuWan849 <wantaku339@gmail.com>
2020-11-19 03:09:06 +0900

    section2にURLをオープンする処理を記述

--- InstaPartProfile/Controllers/SettingsViewController.swift
+++ InstaPartProfile/Controllers/SettingsViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import SafariServices
 /*
 SettingsViewController
  今回はそれぞれのcellがタップしたら処理機能を持つので
@@ -59,7 +59,16 @@ class SettingsViewController: UIViewController {
                 セル1 : (title: "Edit Profile", handler: (Function)),
                 セル2 : (title: "Invited Friends", handler: (Function)),
                 セル3 : (title: "Save Original Posts", handler: (Function)),
-            ]
+            ],
+             セクション2 : [
+                 セル1 : (title: "term", handler: (Function)),
+                 セル2 : (title: "privacy", handler: (Function)),
+                 セル3 : (title: "help", handler: (Function)),
+             ],
+             セクション3 : [
+                 セル1 : (title: "Logout", handler: (Function)),
+             ],
+                
          ]
          */
         
@@ -84,7 +93,25 @@ class SettingsViewController: UIViewController {
         ])
         
         // section2 (terms of service, privacy, help)
+        data.append([
+            SettingsCellModel(title: "Terms of service"){ [weak self] in
+                
+                self?.openURL(type: .term)
+                
+            },
+            SettingsCellModel(title: "Privacy"){ [weak self] in
+                
+                self?.openURL(type: .privacy)
+                
+            },
+            SettingsCellModel(title: "Help"){ [weak self] in
+                
+                self?.openURL(type: .help)
+                
+            }
+        ])
         
+        // section3 LogOut
         print(data)
     }
     
@@ -105,7 +132,36 @@ class SettingsViewController: UIViewController {
     }
     
     // MARK: - Section2
+    /*
+     - 行う処理が全く同じ時(webページの遷移)等はenumを使ってタイプ作って場合分けをする
+     */
+    enum settingsURLType {
+        case term
+        case privacy
+        case help
+    }
     
+    private func openURL(type: settingsURLType) {
+        
+        let urlString : String
+        
+        // typeで分けているときはすべての可能性を考慮に入れる
+        switch type {
+        case .term:
+            urlString = "https://help.instagram.com/581066165581870?ref=dp"
+        case .privacy:
+            urlString = "https://www.instagram.com/terms/accept/?hl=ja"
+        case .help:
+            urlString = "https://help.instagram.com/"
+        }
+        
+        guard  let url = URL(string: urlString) else {
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
     
     // MARK: - Section3
 
